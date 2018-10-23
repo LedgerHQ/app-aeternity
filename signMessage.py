63e05628a6571b0b5b21e7138ccb9023ac68918c
Nikita <nikita.cedrik@gmail.com>
2018-10-23 13:51:37 +1000

    Add example signMessage

---
+++ signMessage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+*******************************************************************************
+*   Ledger Blue
+*   (c) 2016 Ledger
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************
+"""
+from ledgerblue.comm import getDongle
+from ledgerblue.commException import CommException
+import argparse
+import struct
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--acc', help="Account number to sign with")
+parser.add_argument('--message', help="Message to be sign")
+args = parser.parse_args()
+
+if args.acc == None:
+	args.acc = 0
+
+accNumber = struct.pack(">I", int(args.acc))
+messageLength = struct.pack(">I", len(args.message))
+apdu = "e0080000".decode('hex') + chr(len(args.message) + len(accNumber) 
+       + len(messageLength)) + accNumber + messageLength + args.message
+
+dongle = getDongle(True)
+result = dongle.exchange(bytes(apdu))
+
+signature = result[0: 1 + 32 + 32]
+print "Signature " + ''.join(format(x, '02x') for x in signature)
