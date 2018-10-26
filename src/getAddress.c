b0ecd3e387a1cf423076f481e143909642c9471e
Denis Davidyuk <denis_davidyuk@hotmail.com>
2018-10-26 13:35:49 +0200

    Drop useless casting

--- src/getAddress.c
+++ src/getAddress.c
@@ -12,7 +12,7 @@ const bagl_element_t ui_address_nanos[] = {
     {{BAGL_LABELINE    , 0x01,   0,  26, 128,  12, 0, 0, 0        , 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_EXTRABOLD_11px|BAGL_FONT_ALIGNMENT_CENTER, 0}, "address", 0, 0, 0, NULL, NULL, NULL },
 
     {{BAGL_LABELINE    , 0x02,   0,  12, 128,  12, 0, 0, 0        , 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_REGULAR_11px|BAGL_FONT_ALIGNMENT_CENTER, 0}, "Address", 0, 0, 0, NULL, NULL, NULL },
-    {{BAGL_LABELINE    , 0x02,  23,  26,  82,  12, 0x80|10, 0, 0  , 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_EXTRABOLD_11px|BAGL_FONT_ALIGNMENT_CENTER, 26  }, (char*)tmpCtx.addressContext.address, 0, 0, 0, NULL, NULL, NULL },
+    {{BAGL_LABELINE    , 0x02,  23,  26,  82,  12, 0x80|10, 0, 0  , 0xFFFFFF, 0x000000, BAGL_FONT_OPEN_SANS_EXTRABOLD_11px|BAGL_FONT_ALIGNMENT_CENTER, 26  }, tmpCtx.addressContext.address, 0, 0, 0, NULL, NULL, NULL },
 };
 
 static unsigned int ui_address_prepro(const bagl_element_t* element) {
