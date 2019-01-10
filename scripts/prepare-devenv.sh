7bf823d07d18972a9858cc64927bdf53ab1745de
CedrikNikita <nikita.cedrik@gmail.com>
2019-01-10 19:55:03 +1000

    Update Nano S SDK in travis build

--- scripts/prepare-devenv.sh
+++ scripts/prepare-devenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ -d clang-arm-fropi -a -d gcc-arm-none-eabi-5_3-2016q1 -a -d nanos-secure-sdk-nanos-1421 ]; then
+if [ -d clang-arm-fropi -a -d gcc-arm-none-eabi-5_3-2016q1 -a -d nanos-secure-sdk-f9e1c7b8904df2eee0ae7e603f552b876c169334 ]; then
     echo "Devend is already prepared. Exiting."
     exit
 fi
@@ -13,5 +13,5 @@ wget http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.
 tar xf clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.10.tar.xz
 mv clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.10 clang-arm-fropi
 
-wget https://github.com/LedgerHQ/nanos-secure-sdk/archive/nanos-1421.tar.gz
+wget https://github.com/LedgerHQ/nanos-secure-sdk/archive/f9e1c7b8904df2eee0ae7e603f552b876c169334.tar.gz -O nanos-1421.tar.gz
 tar xf nanos-1421.tar.gz
